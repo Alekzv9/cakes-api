1883c652810a5770464ec953cff293aa88318e6e
Erick Valdivia <erick_valdivia@epam.com>
2020-11-02 08:35:33 -0600

    Added service and controller logic

--- controllers/cakes.controller.js
+++ controllers/cakes.controller.js
@@ -6,18 +6,42 @@ const router = express.Router();
 
 router.get(
   '',
-  errorWrap(async (req, res) => {
-    const cakes = await CakesService.getCakes();
-    res.json(new Response('Fetched cakes', cakes));
+  errorWrap(async (_, res) => {
+    const cakes = await CakesService.getAll();
+    return res.json(new Response('Fetched cakes', cakes));
   })
 );
 
-router.get('/:id', (req, res) => {});
+router.get(
+  '/:id',
+  errorWrap(async (req, res) => {
+    const cake = await CakesService.getById(req.params);
+    return res.json(new Response('Fetched cake', cake));
+  })
+);
 
-router.post('', (req, res) => {});
+router.post(
+  '',
+  errorWrap(async (req, res) => {
+    await CakesService.create(req.body);
+    return res.json(new Response('Cake created'));
+  })
+);
 
-router.put('/:id', (req, res) => {});
+router.put(
+  '/:id',
+  errorWrap(async (req, res) => {
+    const cake = await CakesService.update(req.params, req.body);
+    return res.json(new Response('Cake updated', cake));
+  })
+);
 
-router.delete('/:id', (req, res) => {});
+router.delete(
+  '/:id',
+  errorWrap(async (req, res) => {
+    const cakes = await CakesService.delete(req.params);
+    return res.json(new Response('Cake deleted', cakes));
+  })
+);
 
 module.exports = router;
--- services/cakes.service.js
+++ services/cakes.service.js
@@ -12,7 +12,47 @@ const cakes = [
   },
 ];
 
-exports.getCakes = async () => {
-  throw new CustomError('Cakes failed', 400);
+const getByName = (name) => {
+  return cakes.find((cake) => cake.name.toUpperCase() === name.toUpperCase());
+};
+
+exports.getAll = async () => {
   return cakes.slice();
+};
+
+exports.getById = async ({ id }) => {
+  const cake = cakes[id];
+  if (cake) {
+    return cake;
+  } else {
+    throw new CustomError('Cake not found', 404);
+  }
+};
+
+exports.create = async ({ name, price, flavors }) => {
+  const cake = getByName(name);
+  if (cake) {
+    throw new CustomError('Cake duplicated', 400);
+  }
+  cakes.push({ name, price, flavors });
+};
+
+exports.update = async ({ id }, { name, price, flavors }) => {
+  let cake = getByName(name);
+  if (cake) {
+    throw new CustomError('Cake duplicated', 400);
+  }
+  cake = cakes[id];
+  if (!cake) {
+    throw new CustomError('Cake not found', 404);
+  }
+  cake.name = name;
+  cake.price = price;
+  cake.flavors = flavors;
+  return cake;
+};
+
+exports.delete = async ({ id }) => {
+  cakes.splice(id, 1);
+  return cakes;
 };
