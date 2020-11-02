0475d09e1cebc039fc87b1fcd4a1a2078cb6246f
Erick Valdivia <erick_valdivia@epam.com>
2020-11-02 08:52:25 -0600

    Added db configuration

---
+++ db/db-config.js
@@ -0,0 +1,15 @@
+const dbConfig = require('./db-configuration')[process.env.ENVIRONMENT];
+
+const getDbConfig = () => {
+  if (!process.env.ENVIRONMENT)
+    throw new Error(
+      'ENVIRONMENT is undefined. process.env.ENVIRONMENT: ' +
+        process.env.ENVIRONMENT
+    );
+
+  if (!dbConfig) throw new Error('dbConfig is undefined. dbconfig' + dbConfig);
+
+  return dbConfig;
+};
+
+module.exports.db_config = getDbConfig();
