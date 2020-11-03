6b23cbeaf576dc60e926e00c47402c8167d52671
Erick Valdivia <erick_valdivia@epam.com>
2020-11-02 18:11:44 -0600

    Remove unique validator

--- db/schemas/cake.schema.js
+++ db/schemas/cake.schema.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
 
 const cakeSchema = mongoose.Schema({
   name: { type: String, required: true, unique: true },
   price: { type: String },
-  flavors: { type: String },
+  flavors: { type: [String] },
 });
-
-cakeSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Cake', cakeSchema);
